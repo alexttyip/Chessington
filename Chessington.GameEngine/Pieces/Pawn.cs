a5d602187e407cd4ad4a61ca358b6a3f646d8739
Alex Yip <alex.yip@softwire.com>
2021-11-23 12:18:55 +0000

    Completed red-02

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
+        private bool hasMoved;
         public Pawn(Player player)
             : base(player) { }
 
@@ -13,12 +13,22 @@ namespace Chessington.GameEngine.Pieces
             var output = new List<Square>();
             var square = board.FindPiece(this);
 
-            if (Player == Player.White)
-                output.Add(new Square(square.Row - 1, square.Col));
-            else
-                output.Add(new Square(square.Row + 1, square.Col));
+            var playerSign = Player == Player.White ? -1 : 1;
+
+            // Can move 1 square
+            output.Add(new Square(square.Row + 1 * playerSign, square.Col));
+
+            // Can move 2 squares if never moved
+            if (!hasMoved)
+                output.Add(new Square(square.Row + 2 * playerSign, square.Col));
 
             return output;
+        }
+
+        public override void MoveTo(Board board, Square newSquare)
+        {
+            base.MoveTo(board, newSquare);
+            hasMoved = true;
         }
     }
 }
