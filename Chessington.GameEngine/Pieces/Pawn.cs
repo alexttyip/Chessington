14e14357bd916341a3134a564e08ccf98303cf4a
Alex Yip <alex.yip@softwire.com>
2021-11-23 16:20:04 +0000

    Completed red-17

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -10,11 +11,31 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            var output = new List<Square>();
             var square = board.FindPiece(this);
-
             var playerSign = Player == Player.White ? -1 : 1;
 
+            var output = GetAvailableForwardMoves(square, board, playerSign).ToList();
+            
+            foreach(var i in new[]{-1, 1}) {
+                var targetSquare = Square.At(square.Row + 1 * playerSign, square.Col + i);
+                
+                if (!targetSquare.IsOnTheBoard()) continue;
+
+                var targetPiece = board.GetPiece(targetSquare);
+                
+                if (targetPiece == null) continue;
+                
+                if (targetPiece.Player == Player) continue;
+
+                output.Add(targetSquare);
+            }
+
+            return output;
+        }
+
+        private IEnumerable<Square> GetAvailableForwardMoves(Square square, Board board, int playerSign)
+        {
+            var output = new List<Square>();
             var oneSquareAhead = Square.At(square.Row + 1 * playerSign, square.Col);
             if (!oneSquareAhead.IsOnTheBoard() || board.IsOccupied(oneSquareAhead)) {
                 // Square ahead is occupied
